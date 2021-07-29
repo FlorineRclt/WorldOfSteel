da863aaad5bccc3bc5148754f3b50a54294991e5
florinerecalt <florine.recalt@gmail.com>
2021-07-30 00:50:28 +0200

    checkpoint before debugage massif

--- src/Controller/Front/CategoryController.php
+++ src/Controller/Front/CategoryController.php
@@ -11,37 +11,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
-//
-//    /**
-//     * @Route("/categories", name="categoryList")
-//     */
-//    public function categoryList (CategoryRepository $categoryRepository)
-//    {
-//        $categories = $categoryRepository->findAll();
-//
-//        return $this->render('Front/item_list.html.twig', [
-//            'categories' => $categories
-//        ]);
-//    }
-//
-//
-//    /**
-//     * @Route("/categories/{id}", name="categoryShow")
-//     */
-//    public function categoryShow ($id, CategoryRepository $categoryRepository)
-//    {
-//        $category = $categoryRepository->find($id);
-//
-//        // si la categorie n'a pas été trouvée, je renvoie une exception (erreur)
-//        // pour afficher une 404
-//        if (is_null($category)) {
-//            throw new NotFoundHttpException();
-//        }
-//
-//        /*on va chercher la page html twig et on l'interprete dans le navigateur
-//         on lui envoie les données du tableau pour pouvoir travailler dessus */
-//        return $this->render('Front/category_show.html.twig', [
-//            'category' => $category
-//        ]);
-//    }
+
+
+    /**
+     * @Route("/categories/{id}", name="categoryShow")
+     */
+    public function categoryShow ($id, CategoryRepository $categoryRepository)
+    {
+        $category = $categoryRepository->find($id);
+
+        // si la categorie n'a pas été trouvée, je renvoie une exception (erreur)
+        // pour afficher une 404
+        if (is_null($category)) {
+            throw new NotFoundHttpException();
+        }
+
+        /*on va chercher la page html twig et on l'interprete dans le navigateur
+         on lui envoie les données du tableau pour pouvoir travailler dessus */
+        return $this->render('Front/category_show.html.twig', [
+            'category' => $category
+        ]);
+    }
 }
--- src/Controller/Front/ItemController.php
+++ src/Controller/Front/ItemController.php
@@ -4,8 +4,11 @@
 namespace App\Controller\Front;
 
 
+use App\Repository\CategoryRepository;
 use App\Repository\ItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Profiler\Profiler;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ItemController extends AbstractController
@@ -14,13 +17,40 @@ class ItemController extends AbstractController
     /**
      * @Route("/items", name="itemList")
      */
-    public function itemList(ItemRepository $itemRepository)
+    public function itemList(ItemRepository $itemRepository) //CategoryRepository $categoryRepository
     {
+        // j'affiche dynamiquement les catégories sur la page des petites annonces
+        // en allant récuperer les données grâce au CategoryRepository et au findAll
+        // qui permet de faire une requête Select en SQL
+//        $categories = $categoryRepository->findAll();
         $items = $itemRepository->findAll();
 
         return $this->render('Front/item_list.html.twig', [
-            'items' => $items
+            'items' => $items,
+//            'categories' => $categories
         ]);
+    }
+
+
+    /**
+     * @Route("/items/{id}", name="itemShow")
+     */
+    public function itemShow($id, ItemRepository $itemRepository) //?Profiler $profiler
+    {
+//        if (null !== $profiler) {
+//            $profiler->disable();
+//        }
+        $item = $itemRepository->find($id);
+
+        if (is_null($item)) {
+            throw new NotFoundHttpException();
+        }
+
+        return $this->render('Front/item_show.html.twig', [
+            'item' => $item
+        ]);
+
+
     }
 
 
